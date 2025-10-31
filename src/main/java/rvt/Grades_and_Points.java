de30a0dbf3ff3b143ca8dee14373bd84e1ef208a
24DP4DGenc <a240628dg@rvt>
2025-10-31 10:18:43 +0200

    grades_uzdevums

---
+++ src/main/java/rvt/Grades_and_Points.java
@@ -0,0 +1,32 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Grades_and_Points {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give points");
+        int points = scanner.nextInt();
+
+        if (points < 0) {
+            System.out.println("neiespējams");
+        } else if (points < 50) {
+            System.out.println("failed");
+        } else if (points < 60) {
+            System.out.println("1");
+        } else if (points < 70) {
+            System.out.println("2");
+        } else if (points < 80) {
+            System.out.println("3");
+        } else if (points < 90) {
+            System.out.println("4");
+        } else if (points < 100) {
+            System.out.println("5");
+        } else {
+            System.out.println("incredible!");
+        }
+    }
+}
+
+
