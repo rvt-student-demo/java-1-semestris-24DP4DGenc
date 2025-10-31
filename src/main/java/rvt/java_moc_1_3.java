031f1760bda6b17501fe707aed958748d037058a
24DP4DGenc <a240628dg@rvt>
2025-10-31 09:39:54 +0200

    1.3 uzdevums

---
+++ src/main/java/rvt/java_moc_1_3.java
@@ -0,0 +1,15 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class java_moc_1_3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+    }
+}
