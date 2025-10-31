c28e43b2b960535ed4d4d0e90f6734333af0b731
24DP4DGenc <a240628dg@rvt>
2025-10-31 10:34:38 +0200

    Rename ArrayList task file

--- src/main/java/rvt/JMArrayList.java
+++ src/main/java/rvt/JMArrayList.java
@@ -3,17 +3,24 @@ package rvt;
 import java.util.*; // importējam vairākas klases
 
 // Katrs uzdevums ir ievietots attiecīgā metodē
-public class array_list {
+public class JMArrayList {
     public static void main(String[] args) {
-        
+
     }
-    public static void onlyTheseNumbers() {}
+
+    public static void onlyTheseNumbers() {
+    }
+
     public static void listSize() {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<>();
         System.out.println("Ievada vārdu");
     }
-public static void onThelist() {}
-public static void removelast(ArrayList<String> strings) {}
+
+    public static void onThelist() {
+    }
+
+    public static void removelast(ArrayList<String> strings) {
+    }
 
 }
