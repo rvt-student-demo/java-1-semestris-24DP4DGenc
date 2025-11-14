59e8becb018688b579a50cd12f241ec39b0ed54f
24DP4DGenc <a240628dg@rvt>
2025-11-14 10:48:45 +0200

    2.2 uzdevums

---
+++ src/main/java/rvt/there_yet.java
@@ -0,0 +1,16 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class there_yet {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Give a number");
+            int input = scanner.nextInt();
+            if (input == 4) {
+                break;
+            }
+        }
+    }
+}
