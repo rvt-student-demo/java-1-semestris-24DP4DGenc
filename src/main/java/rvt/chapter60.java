1e04c0bb339ccde77e801587e4faba8a2cd37b1e
24DP4DGenc <a240628dg@rvt>
2025-11-27 10:21:17 +0200

    chapter60.java

--- src/main/java/rvt/chapter60.java
+++ src/main/java/rvt/chapter60.java
@@ -55,10 +55,16 @@ public class chapter60 {
                     }
                             public static void Exercise5() {
                                     int[] val = {0, 1, 2, 3}; 
-                                    int temp;
+                                    int temp = val[1];
  
                                 System.out.println( "Original Array: " 
                                 + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+
+                                val[0] = val[3];
+                                val[1] = val[2];
+                                val[2] = temp;
+                                val[3] = temp - 1;
+
 
   
                                 System.out.println( "Reversed Array: " 
