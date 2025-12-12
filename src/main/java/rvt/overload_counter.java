b045675497012eb4ce356920b318e0a2a0c7b010
24DP4DGenc <a240628dg@rvt>
2025-12-12 09:50:37 +0200

    overload_counter

---
+++ src/main/java/rvt/overload_counter.java
@@ -0,0 +1,38 @@
+package rvt;
+
+public class overload_counter {
+
+    private int value;
+
+    public overload_counter(int startValue) {
+        this.value = startValue;
+    }
+
+    public overload_counter() {
+        this.value = 0;
+    }
+
+    public int value() {
+        return this.value;
+    }
+
+    public void increase() {
+        this.value++;
+    }
+
+    public void decrease() {
+        this.value--;
+    }
+
+    public void increase(int increaseBy) {
+        if (increaseBy >= 0) {
+            this.value += increaseBy;
+        }
+    }
+
+    public void decrease(int decreaseBy) {
+        if (decreaseBy >= 0) {
+            this.value -= decreaseBy;
+        }
+    }
+}
